e4000c06a2dae29f12b5286c2704332667537a41
luckonis <lucas.koenig@epitech.eu>
2026-04-17 10:13:09 +0200

    Clear warning

--- backend/backend.Domain/Entities/Workspace.cs
+++ backend/backend.Domain/Entities/Workspace.cs
@@ -3,14 +3,14 @@ namespace backend.Domain.Entities;
 public class Workspace
 {
   public int Id { get; set; }
-  public string Name { get; set; }
+  public string Name { get; set; } = string.Empty;
   public string? Description { get; set; }
 
   public DateTimeOffset CreatedAt { get; set; } = DateTimeOffset.UtcNow;
   public DateTimeOffset UpdatedAt { get; set; } = DateTimeOffset.UtcNow;
 
   public int UserId { get; set; }
-  public User User { get; set; }
+  public User User { get; set; } = null!;
 
   public List<Page> Pages { get; set; } = new();
 }
